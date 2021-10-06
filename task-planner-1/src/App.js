261cdcfd19036e2c3993c3a02d96e269c02344a1
Alejandro Toro Daza <54339952+Skullzo@users.noreply.github.com>
2021-10-05 21:38:04 -0500

    Daily Routine updated

--- task-planner-1/src/App.js
+++ task-planner-1/src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import { TaskItem } from "./TaskItem";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      isCompleted: false,
+      name: "Wake up.",
+    },
+    {
+      isCompleted: false,
+      name: "Take a shower.",
+    },
+    {
+      isCompleted: false,
+      name: "Make the bed.",
+    },
+    {
+      isCompleted: false,
+      name: "Eat some breakfast.",
+    },
+    {
+      isCompleted: false,
+      name: "Go to school.",
+    },
+    {
+      isCompleted: false,
+      name: "Go to Math class.",
+    },    
+    {
+      isCompleted: false,
+      name: "Play Soccer.",
+    },
+    {
+      isCompleted: false,
+      name: "Go Home.",
+    },
+    {
+      isCompleted: false,
+      name: "Do Math homework.",
+    },
+    {
+      isCompleted: false,
+      name: "Have some dinner.",
+    },
+    {
+      isCompleted: false,
+      name: "Go to sleep.",
+    }
+  ])
+  const [textValue, setTextValue] = useState("");
+  const handleTaskChange = (index) => () => {
+    const arr = [...tasks];
+    arr[index].isCompleted = !arr[index].isCompleted;
+    setTasks(arr);
+  };
+  const newTask = (name) => {
+    const newTask = {
+      isCompleted: false,
+      name: name,
+    };
+    setTasks([...tasks, newTask]);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(textValue);
+    newTask(textValue);
+  };
+  const handleTextChange = (event) => {
+    const value = event.target.value;
+    setTextValue(value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <br></br>
+        Daily routine<br></br>
+        <br></br>
+        <input 
+          value={textValue}
+          onChange={handleTextChange}
+          type="text"
+          placeholder="Type new activity"
+        />
+        <button>Create new Activity</button>        
+      </form>
+      <ul>
+        {tasks.map((task,index) => {
+          return (
+            <TaskItem
+              isChecked={task.isCompleted}
+              taskName={task.name}
+              onTaskChange={handleTaskChange(index)}
+            />
+          );
+        })}
+      </ul>
+    </main>
   );
 }
 
